64188c8c185b5c532038f1f58f8c319bae364e41
forecho <caizhenghai@gmail.com>
2021-08-20 17:21:16 +0800

    fixed

--- src/app/shared/shared-delon.module.ts
+++ src/app/shared/shared-delon.module.ts
@@ -1,23 +1,22 @@
 import { DownFileModule } from '@delon/abc/down-file';
 import { ExceptionModule } from '@delon/abc/exception';
 import { GlobalFooterModule } from '@delon/abc/global-footer';
+import { LoadingModule } from '@delon/abc/loading';
 import { NoticeIconModule } from '@delon/abc/notice-icon';
 import { PageHeaderModule } from '@delon/abc/page-header';
+import { QRModule } from '@delon/abc/qr';
 import { ResultModule } from '@delon/abc/result';
 // import { ReuseTabModule } from '@delon/abc/reuse-tab';
 import { SEModule } from '@delon/abc/se';
-// import { SidebarNavModule } from '@delon/abc/sidebar-nav';
 import { STModule } from '@delon/abc/st';
 import { SVModule } from '@delon/abc/sv';
 import { TagSelectModule } from '@delon/abc/tag-select';
 import { XlsxModule } from '@delon/abc/xlsx';
+import { ChartEChartsModule } from '@delon/chart/chart-echarts';
 import { G2CustomModule } from '@delon/chart/custom';
 import { G2MiniProgressModule } from '@delon/chart/mini-progress';
 import { NumberInfoModule } from '@delon/chart/number-info';
 import { TrendModule } from '@delon/chart/trend';
-import { QRModule } from '@delon/abc/qr';
-import { LoadingModule } from '@delon/abc/loading';
-import { ChartEChartsModule } from '@delon/chart/chart-echarts';
 import { CurrencyPipeModule } from '@delon/util/pipes/currency';
 
 export const SHARED_DELON_MODULES = [
@@ -25,7 +24,6 @@ export const SHARED_DELON_MODULES = [
   ResultModule,
   ExceptionModule,
   NoticeIconModule,
-  // SidebarNavModule,
   GlobalFooterModule,
   STModule,
   SEModule,
@@ -40,6 +38,6 @@ export const SHARED_DELON_MODULES = [
   QRModule,
   LoadingModule,
   ChartEChartsModule,
-  CurrencyPipeModule,
+  CurrencyPipeModule
   // ReuseTabModule,
 ];
